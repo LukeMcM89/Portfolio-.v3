c127cc64153fba42ee6878f2e7710a19120f10d8
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-04 14:49:16 -0400

    Retrofitting Navbar elements into React portfolio

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import styles from 
+import styles from '../styles/Layout'
 
 const Layout = props => {
     return (
---
+++ src/pages/header.js
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+//about me + portfolio + contact 
+
+const Header = () => (
+    <>
+    <nav><nav class="navbar is-black" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="index.html">
+                    <img src="">
+                </a>
+            </div>
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                data-target="portfolioBasicBar"></a>
+            <div id="portfolioBasicBar" class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item" href="index.html">
+                        Home
+                    </a>
+                    <a class="navbar-item" href="my-work.html">
+                        Portfolio
+                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a class="navbar-link">
+                            More
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item" href="contact.html">
+                                Contact Me
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="navbar-end">
+            </div>
+        </nav></nav>
+    </>
+)
