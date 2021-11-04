60447418d9ab14ed115dc6c4367f17a372cca5d6
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-04 15:44:54 -0400

    Importing portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import Header from './pages/Header';
 import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
      <Header/>
      <About/>
-     
+     <Portfolio/>
     </div>
   );
 }
