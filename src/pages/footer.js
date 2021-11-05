3a1f72a7ed3c2c260f25ccd9d14826dd2c3133d1
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-11-05 14:21:50 -0400

    Footer debug

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -4,8 +4,8 @@ import Layout from '../components/Layout'
 const Footer = () => (
     <footer class="footer has-background-black">
     <div class="content has-text-centered has-background-black">
-        <p class="has-text-white subtile is-size-4"><em> The expert at anything was once a beginner - Helen Hays</em><p>  
-        <p class="has-text-white subtile is-size-4"><address=</p>>E-mail: <a href="mailto:lmcmurtrie89@gmail.com">Lmcmurtrie89@gmail.com</a></address></p></p>   
+        <p class="has-text-white subtile is-size-4"><em> The expert at anything was once a beginner - Helen Hays</em></p>  
+        <p class="has-text-white subtile is-size-4"><address>E-mail: <a href="mailto:lmcmurtrie89@gmail.com">Lmcmurtrie89@gmail.com</a></address></p>  
         <p class="has-text-white subtile is-size-5">Cell: (804) 432 - 6049</p>  
     </div>
 </footer>
